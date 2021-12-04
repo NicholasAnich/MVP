9667f5fe3d23afb628ba0c4e4fddc0856109aec4
NicholasAnich <infinitevolume@gmail.com>
2021-12-04 07:40:04 -0800

    refactored port to be process.env.PORT

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 const router = express.Router();
 
-app.use(express.static('./'))
+app.use(express.static('./'));
 app.use('/', router);
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Listening on port:${port}`)
 })
